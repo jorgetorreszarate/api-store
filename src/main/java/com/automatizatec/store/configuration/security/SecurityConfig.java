af10d1c1b7e416de8da7d11f6a06bb562ec49872
Jorge Luis Torres Zárate <jtorreszarate2405@gmail.com>
2025-08-31 11:23:52 -0500

    Actualizaciones

--- src/main/java/com/automatizatec/store/configuration/security/SecurityConfig.java
+++ src/main/java/com/automatizatec/store/configuration/security/SecurityConfig.java
@@ -5,14 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -41,17 +38,5 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // Puedes personalizar los parámetros de Argon2 o dejar los defaults
-        return new Argon2PasswordEncoder(
-                16,    // saltLength (recomendado >= 16)
-                32,    // hashLength (recomendado >= 32)
-                1,     // parallelism (número de hilos)
-                1 << 12, // memory (en KB, 4096 KB = 4MB)
-                3      // iterations (vueltas de cálculo)
-        );
     }
 }
